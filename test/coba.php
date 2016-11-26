220a95cb6af8b3f3fe9b08cdf353f8eff1891d99
puntonugroho <punugroho@gmail.com>
2016-11-26 12:40:37 +0700

    Mantappp

---
+++ test/coba.php
@@ -0,0 +1,24 @@
+<?php
+	$opts = array(
+			'http'=>array(
+				'method'=>"GET",
+				'header'=>"Accept: application/json\r\n" .
+						"X-Kurio-Client-ID: 99\r\n" .
+						"X-Kurio-Client-Secret: S3VyaW9IYWNrYXRvbjIw\r\n" .
+						"X-OS: windows\r\n" .
+						"X-App-Version: 1.0\r\n" .
+						"Authorization: Bearer YKtzmrkSKD6rvesV9e7PfPuKSNlFCIWg9Acq5G2Z\r\n"
+				)
+		);
+	$context=stream_context_create($opts);
+
+	$url='https://hack.kurio.co.id/v1/feed/top_stories';
+
+	$content=file_get_contents($url,false,$context);
+	$json= json_decode($content,true);
+
+	foreach($json['data'] as $item){
+		echo $item['title'];
+		echo '<br>';
+	}
+?>
