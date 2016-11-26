9b7bcfa1e64d354ce856364c657214a2b8b34cda
crashno <kresno.19@gmail.com>
2016-11-27 01:04:38 +0700

    ini single

--- crashno/single.php
+++ crashno/single.php
@@ -3,89 +3,14 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Free Bootstrap Themes by 365Bootstrap dot com - Free Responsive Html5 Templates">
-    <meta name="author" content="http://www.365bootstrap.com">
-	
-     <title>Hackathon Kurio 2016</title>
-	
-    <!-- Bootstrap Core CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css"  type="text/css">
-	
-	<!-- Custom CSS -->
-    <link rel="stylesheet" href="css/style.css">
-	
-	<!-- Owl Carousel Assets -->
-    <link href="owl-carousel/owl.carousel.css" rel="stylesheet">
-    <link href="owl-carousel/owl.theme.css" rel="stylesheet">
-	
-	<!-- Custom Fonts -->
-    <link rel="stylesheet" href="font-awesome-4.4.0/css/font-awesome.min.css"  type="text/css">
-	
-	<!-- jQuery and Modernizr-->
-	<script src="js/jquery-2.1.1.js"></script>
-	
-	<!-- Core JavaScript Files -->  	 
-    <script src="js/bootstrap.min.js"></script>
-	
-	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="js/html5shiv.js"></script>
-        <script src="js/respond.min.js"></script>
-    <![endif]-->
-</head>
 
 <body>
-<header>
-	<div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Hackathon Kurio 2K16</a>
-        </div>
-        <div class="navbar-collapse collapse">
-        	<ul class="nav navbar-nav">
-				<li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		            <li role="separator" class="divider"></li>
-		            <li><a href="#">Separated link</a></li>
-		            <li role="separator" class="divider"></li>
-		            <li><a href="#">One more separated link</a></li>
-		          </ul>
-		        </li>
-			</ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-	          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Akun <span class="caret"></span></a>
-	          <ul class="dropdown-menu">
-	            <li><a href="#loginModal" data-toggle='modal'>Login</a></li>
-	            <li role="separator" class="divider"></li>
-	            <li><a href="#daftarModal" data-toggle='modal'>Daftar</a></li>
-	          </ul>
-	        </li>
-          </ul>
-        </div>
-      </div>
-    </div>  
-</header>
+	<?php include 'navbar.php'; ?>
 	
 	<!-- /////////////////////////////////////////Content -->
 	<div id="page-content" class="single-page container">
 		<div class="row">
-			<div id="main-content" class="col-md-8">
+			<div id="main-content" class="col-md-9">
 				<div class="box" style="border:0;">
 					<div class="thumbnail">
 				      <h1 class="article-name"><?php echo $json['title']; ?></h1>
@@ -105,7 +30,6 @@
 				      <img src="<?php echo $json['thumbnail']['url']; ?>">
 					  <div class="caption">
 					        <div class="line"></div>
-						<h1 class="vid-name">Deskripsi Berita</h1>
 						<?php echo $json['content'][1]['text']; ?>
 						<div class="line"></div>
 				      </div>
@@ -176,6 +100,35 @@
 										<span><i class="fa fa-calendar"></i>25/3/2015</span> 
 										<span><i class="fa fa-heart"></i>1,200</span>
 									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="sidebar">
+				<div class="col-md-3">
+					<!---- Start Widget ---->
+					<div class="widget wid-gallery">
+						<div class="heading"><h4>Top Stories</h4></div>
+						<div class="content">
+							<div class="col-md-12">
+								<div class="row">
+									<a href="single.php?id=<?php echo $temp[0]['id']; ?>"><img src="<?php echo $temp[0]['gambar'] ?>" />
+									<h5 class="top-name">
+									<br>
+										<a href="single.php?id=<?php echo $temp[0]['id']; ?>"><?php echo $temp[0]['judul']; ?></a>
+									</h5>
+
+									<div class="top-name">
+										<hr class="style-line">
+										<a href="single.php?id=<?php echo $temp[1]['id']; ?>"><?php echo $temp[1]['judul']; ?></a><hr class="style-line"> 
+										<a href="single.php?id=<?php echo $temp[2]['id']; ?>"><?php echo $temp[2]['judul']; ?></a><hr class="style-line"> 
+										<a href="single.php?id=<?php echo $temp[3]['id']; ?>"><?php echo $temp[3]['judul']; ?></a><hr class="style-line"> 
+										<a href="single.php?id=<?php echo $temp[4]['id']; ?>"><?php echo $temp[4]['judul']; ?></a><hr class="style-line"> 
+										<a href="single.php?id=<?php echo $temp[5]['id']; ?>"><?php echo $temp[5]['judul']; ?></a><hr class="style-line">
+									</div>
+
 								</div>
 							</div>
 						</div>
