eea5b30be81f18b879518d90026568e2185353f7
Ryan Baskara <ryan.baskara@gmail.com>
2016-11-27 08:57:03 +0700

    oke

--- crashno/logout.php
+++ crashno/logout.php
@@ -3,5 +3,7 @@
     setcookie('login', null, -1, '/');
     unset($_COOKIE['jenis_kelamin']);
     setcookie('jenis_kelamin', null, -1, '/');
+    nset($_COOKIE['name']);
+    setcookie('name', null, -1, '/');
     header('location:index.php');
  ?>
